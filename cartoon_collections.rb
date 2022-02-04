df9e0bdf72331123945a42d09916aea9d4f2b807
Christy Perozzi <christy.perozzi@gmail.com>
2022-02-04 16:08:59 -0500

    commit tree 795dd1ef120a57c14039004a34db8d1e80d31666

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+  arr.each_with_index { |dwarf, index| puts "#{index+1}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map{ |x| x.capitalize() + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.find{ |x| x.length > 4 } ? true : false 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.each  {|x| 
+    if cheese_types.include?(x)
+      return x
+    end
+}
+nil
 end
+
+#roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
+#puts summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
+#puts long_planeteer_calls(["wind", "fire", "ear"])
+puts find_the_cheese(["crackers", "goudaa", "thyme"])
